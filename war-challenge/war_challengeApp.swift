2c05502c0d54d94a89e08c9597c52d780aa98bea
Markus Flodmark <markus.flodmark@gmail.com>
2021-01-02 10:24:13 +0100

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Markus Flodmark on 2021-01-02.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
